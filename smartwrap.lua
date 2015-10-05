090e8a2041b6858c5f900480a72805813da1f7dd
Ben Tucker <nenofite@gmail.com>
2015-10-05 00:57:34 -0700

    init

---
+++ smartwrap.lua
@@ -0,0 +1,232 @@
+local lpeg = require 'lpeg'
+
+---
+-- Check whether the given argument is of the expected type, throwing an error
+-- if it is not.
+--
+-- @tparam string name The name of the argument, to be used in the error
+-- message.
+--
+-- @param arg The argument to check.
+--
+-- @tparam string expected_type The string form of the expected type, as
+-- returned by the builtin function @{type}.
+--
+-- @tparam boolean optional (optional) Whether the argument is optional. If
+-- the argument is optional, then nil is an accepted value. Defaults to false.
+function arg(name, arg, expected_type, optional)
+  local bad = type(arg) ~= expected_type
+  
+  if bad and optional and arg == nil then
+    bad = false
+  end
+
+  if bad then
+    local message = "The argument `" .. name .. "` must be of the type " ..
+      expected_type
+    
+    if optional and expected_type ~= 'nil' then
+      message = message .. " or nil"
+    end
+    
+    error(message .. ".")
+  end
+end
+
+---
+-- Separate the lines in the text into a list.
+--
+-- @tparam string text The text to split.
+--
+-- @treturn table A list of lines, none of which contain newlines.
+function split_lines(text)
+  local result = {}
+
+  local start_index = 1
+  
+  while true do
+    local stop_index = string.find(text, '\n', start_index)
+    
+    if stop_index then
+      table.insert(result, string.sub(text, start_index, stop_index - 1))
+      start_index = stop_index + 1
+    else
+      table.insert(result, string.sub(text, start_index, -1))
+      break
+    end
+  end
+  
+  return result
+end
+
+---
+-- Split the text into lines such that no line exceeds the maximum line length.
+--
+-- @tparam string text The text to wrap.
+--
+-- @tparam number max_line_length The maximum length of a single line. This
+-- must be at least 2.
+--
+-- @treturn table A list of lines of text.
+function wrap_text(text, max_line_length)
+  arg('text', text, 'string')
+  arg('max_line_length', max_line_length, 'number')
+  
+  assert(
+    max_line_length >= 2,
+    "The argument `max_line_length` must be at least 2."
+  )
+  
+  local result = {}
+  
+  local current_line = ''
+	
+	local function add_word(word, is_second_try)
+		local candidate_length = #word
+		local add_space = #current_line ~= 0
+    
+		if add_space then
+			candidate_length = candidate_length + 1
+		end
+		
+		if #current_line + candidate_length < max_line_length then
+			if add_space then
+				current_line = current_line .. ' ' .. word
+			else
+				current_line = current_line .. word
+			end
+		else
+			if is_second_try then
+        current_line = word
+			else
+				if #current_line > 0 then
+          table.insert(result, current_line)
+          current_line = ''
+        end
+        
+				add_word(word, true)
+			end
+		end
+	end
+	
+	for word in string.gmatch(text, '[^ \t\n\r]+') do
+		add_word(word)
+	end
+  
+  if #current_line > 0 then
+    table.insert(result, current_line)
+  end
+	
+	return result
+end
+
+local line_prefix_peg = lpeg.C(
+  (lpeg.P(1) - (lpeg.R'az' + lpeg.R'AZ' + lpeg.R'09')) ^ 0
+)
+
+---
+-- Determine what portion of this line is the prefix and return it.
+--
+-- @tparam string line The line from which to extract the prefix. It must
+-- contain no newlines.
+--
+-- @treturn string The prefix of the given line.
+function extract_prefix(line)
+  arg('line', line, 'string')
+  
+  return line_prefix_peg:match(line)
+end
+
+---
+-- Determine whether this line could be considered part of a paragraph.
+--
+-- @tparam string line The line to consider. It must contain no newlines. It
+-- still contains the prefix.
+--
+-- @tparam string prefix The prefix of this line, extracted by the function
+-- @{extract_prefix}.
+--
+-- @treturn boolean Whether the line could be part of a paragraph.
+function is_within_paragraph(line, prefix)
+  arg('line', line, 'string')
+  arg('prefix', prefix, 'string')
+
+  return #prefix ~= #line
+end
+
+---
+-- Split the lines of text apart such that no line exceeds the maximum line
+-- length, while preserving individual paragraphs.
+--
+-- Text will only wrap within the same paragraph. Lines between paragraphs
+-- will be preserved.
+--
+-- @tparam string text The text to wrap.
+--
+-- @tparam number max_line_length The maximum length of a line of text.
+--
+-- @treturn string The wrapped text.
+function wrap_paragraphs(text, max_line_length)
+  arg('text', text, 'string')
+  arg('max_line_length', max_line_length, 'number')
+
+  local lines = split_lines(text)
+  
+  local result_lines = {}
+  
+  local num_lines = #lines
+  local i = 1
+  
+  -- Loop through each line of text.
+  while i <= num_lines do
+    local line = lines[i]
+  
+    local prefix = extract_prefix(line)
+    if is_within_paragraph(line, prefix) then
+      -- If the line is a paragraph, loop through the remaining lines until we find
+      -- the end of the paragraph or the end of the input text. Remove the prefix
+      -- from each line and add it to a temporary buffer.
+      local paragraph = ''
+      while i <= num_lines do
+        line = lines[i]
+        
+        local prefix2 = extract_prefix(line)
+        if is_within_paragraph(line, prefix2) then
+          paragraph = paragraph .. '\n' .. string.sub(line, #prefix2 + 1)
+          i = i + 1
+        else
+          i = i - 1
+          break
+        end
+      end
+      
+      -- Wrap the buffer, discounting the length of the prefix from the maximum
+      -- line length.
+      local wrapped_lines = wrap_text(paragraph, max_line_length - #prefix)
+      
+      -- Loop through the results of the wrap, prepending them with the prefix and
+      -- adding the resulting line to our results.
+      for _, wrapped_line in ipairs(wrapped_lines) do
+        table.insert(result_lines, prefix .. wrapped_line)
+      end
+    else
+      -- If the line is non-paragraph, pass it through to the results without
+      -- modification.
+      table.insert(result_lines, line)
+    end
+    
+    i = i + 1
+  end
+  
+  if #result_lines == 0 then
+    return ''
+  end
+  
+  local result = result_lines[1]
+  
+  for i = 2, #result_lines do
+    result = result .. '\n' .. result_lines[i]
+  end
+  
+  return result
+end
---
+++ spec.lua
@@ -0,0 +1,84 @@
+require('busted.runner')()
+
+require 'smartwrap'
+
+local long_text = [[
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus porttitor, ante eget cursus varius, dui odio placerat lacus, a ultricies nisl purus non mauris. Sed fermentum leo ipsum, eu fringilla libero ullamcorper id. Nullam vitae magna aliquam, placerat tortor ut, semper tortor. Nulla porttitor lobortis hendrerit. Nam eget lectus luctus, pellentesque dolor eu, dignissim nisi. Suspendisse commodo accumsan mi, eget suscipit tortor ultrices id. Maecenas interdum interdum augue, at dapibus nisl. Sed at dolor ut odio imperdiet semper. Morbi sed est eu ante pharetra ultricies eget faucibus urna. Praesent at bibendum lacus, quis sagittis massa. Duis ac vestibulum enim. Nulla.]]
+
+local long_text_wrapped_40 = [[
+Lorem ipsum dolor sit amet, consectetur
+adipiscing elit. Phasellus porttitor,
+ante eget cursus varius, dui odio
+placerat lacus, a ultricies nisl purus
+non mauris. Sed fermentum leo ipsum, eu
+fringilla libero ullamcorper id. Nullam
+vitae magna aliquam, placerat tortor
+ut, semper tortor. Nulla porttitor
+lobortis hendrerit. Nam eget lectus
+luctus, pellentesque dolor eu,
+dignissim nisi. Suspendisse commodo
+accumsan mi, eget suscipit tortor
+ultrices id. Maecenas interdum interdum
+augue, at dapibus nisl. Sed at dolor ut
+odio imperdiet semper. Morbi sed est eu
+ante pharetra ultricies eget faucibus
+urna. Praesent at bibendum lacus, quis
+sagittis massa. Duis ac vestibulum
+enim. Nulla.]]
+
+local paragraphs = [[
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus porttitor, ante eget
+cursus
+varius,
+dui
+odio.
+
+> Ante pharetra ultricies eget faucibus urna. Praesent at bibendum lacus, quis
+> sagittis massa. Duis ac vestibulum.
+
+
+]]
+
+local paragraphs_wrapped = [[
+Lorem ipsum dolor sit amet, consectetur
+adipiscing elit. Phasellus porttitor,
+ante eget cursus varius, dui odio.
+
+> Ante pharetra ultricies eget faucibus
+> urna. Praesent at bibendum lacus,
+> quis sagittis massa. Duis ac
+> vestibulum.
+
+
+]]
+
+describe("The function `wrap_text`", function()
+  it("eliminates extra whitespace.", function()
+    local lines = wrap_text("  \t \n", 40)
+  
+    assert.is_table(lines)
+    assert.is_equal(0, #lines)
+  end)
+
+  it("wraps to the specified maximum line length.", function()
+    local lines = wrap_text(long_text, 40)
+    
+    local lines_joined = ''
+    
+    for _, line in ipairs(lines) do
+      lines_joined = lines_joined .. line .. '\n'
+    end
+  
+    assert.is_equal(long_text_wrapped_40 .. '\n', lines_joined)
+  end)
+end)
+
+describe("The function `wrap_paragraphs`", function()
+  it("works the same as `wrap_text` for single-paragraph input text.", function()
+    assert.is_equal(long_text_wrapped_40, wrap_paragraphs(long_text, 40))
+  end)
+  
+  it("wraps multiple-paragraph text.", function()
+    assert.is_equal(paragraphs_wrapped, wrap_paragraphs(paragraphs, 40))
+  end)
+end)
